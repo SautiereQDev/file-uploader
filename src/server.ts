005e8baac3b6f02c5af864b312cdc05995faf8d3
SautiereQDev <contact@quentinsautiere.com>
2025-03-26 18:19:41 +0100

    update project name and version; enhance server startup with port finding and error logging

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,20 @@
-import { app } from "./app";
+import {app} from "./app";
+import {LoggerService} from "./services/loggerService";
+import {findAvailablePort} from "./utils/portFinder";
 
-const PORT = process.env.PORT ?? 3002;
+const PORT: number = parseInt(process.env.PORT ?? "3002", 10);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+async function startServer(): Promise<void> {
+  try {
+    const port = await findAvailablePort(PORT);
+
+    app.listen(port, () => {
+      LoggerService.info(`Server start on port ${port}`);
+    });
+  } catch (error) {
+    LoggerService.error('Error during server initialisation', {error});
+    process.exit(1);
+  }
+}
+
+startServer().catch((e) => LoggerService.error('Error during server initialisation', e));
